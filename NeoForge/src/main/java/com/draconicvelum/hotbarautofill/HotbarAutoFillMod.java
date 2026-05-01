ecb687dc7b166d78a5afbbd4d6faf0012ca1d00d
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-05-01 12:16:27 +0100

    Refactor HotbarAutoFillMod constructor and methods

--- NeoForge/src/main/java/com/draconicvelum/hotbarautofill/HotbarAutoFillMod.java
+++ NeoForge/src/main/java/com/draconicvelum/hotbarautofill/HotbarAutoFillMod.java
@@ -1,7 +1,11 @@
 package com.draconicvelum.hotbarautofill;
 
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RenderGuiEvent;
 import net.neoforged.neoforge.common.NeoForge;
@@ -9,36 +13,27 @@ import net.neoforged.neoforge.common.NeoForge;
 @Mod(value = HotbarAutoFillMod.MOD_ID, dist = Dist.CLIENT)
 public final class HotbarAutoFillMod {
 	static final String MOD_ID = "hotbarautofill";
-	private static boolean configured;
 
-	public HotbarAutoFillMod() {
+	public HotbarAutoFillMod(ModContainer container) {
+		container.registerConfig(ModConfig.Type.CLIENT, HotbarAutoFillConfig.CLIENT_SPEC);
+		container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 		NeoForge.EVENT_BUS.addListener(this::onClientTick);
 		NeoForge.EVENT_BUS.addListener(this::onRenderGui);
 	}
 
 	private void onClientTick(ClientTickEvent.Pre event) {
-		if (!ensureConfigured()) {
-			return;
-		}
+		configure();
 		HotbarRefill.tick(net.minecraft.client.Minecraft.getInstance());
 	}
 
 	private void onRenderGui(RenderGuiEvent.Post event) {
-		if (!ensureConfigured()) {
-			return;
-		}
+		configure();
 		HotbarItemCounter.render(event.getGuiGraphics());
 	}
 
-	private static boolean ensureConfigured() {
-		if (configured || net.minecraft.client.Minecraft.getInstance() == null) {
-			return configured;
-		}
-
+	private static void configure() {
 		HotbarAutoFillConfig config = HotbarAutoFillConfig.load();
 		HotbarRefill.configure(config);
 		HotbarItemCounter.configure(config);
-		configured = true;
-		return true;
 	}
 }
