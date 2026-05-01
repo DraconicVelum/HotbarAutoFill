7ba0742ffd34209066f5332aa030119f00091df9
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-05-01 11:25:08 +0100

    Integrate item counter into HUD and configure
    Added item counter to HUD and configured it with the existing hotbar autofill settings.

--- Fabric/src/client/java/com/draconicvelum/hotbarautofill/HotbarAutoFillMod.java
+++ Fabric/src/client/java/com/draconicvelum/hotbarautofill/HotbarAutoFillMod.java
@@ -2,12 +2,24 @@ package com.draconicvelum.hotbarautofill;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.minecraft.resources.Identifier;
 
 public final class HotbarAutoFillMod implements ClientModInitializer {
+	private static final Identifier ITEM_COUNTER_ID = Identifier.parse("hotbarautofill:item_counter");
+
 	@Override
 	public void onInitializeClient() {
-		HotbarRefill.configure(HotbarAutoFillConfig.load());
+		HotbarAutoFillConfig config = HotbarAutoFillConfig.load();
+		HotbarRefill.configure(config);
+		HotbarItemCounter.configure(config);
 		ClientTickEvents.START_CLIENT_TICK.register(HotbarAutoFillMod::onClientTick);
+		HudElementRegistry.attachElementAfter(
+				VanillaHudElements.HOTBAR,
+				ITEM_COUNTER_ID,
+				(graphics, tickCounter) -> HotbarItemCounter.render(graphics)
+		);
 	}
 
 	private static void onClientTick(net.minecraft.client.Minecraft client) {
