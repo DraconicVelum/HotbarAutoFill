608a7d83259d62c1a5e2a50c90125e5a92699b08
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-05-01 12:15:40 +0100

    Refactor HotbarAutoFillConfig into HotbarAutoFillMod

--- NeoForge/src/main/java/com/draconicvelum/hotbarautofill/HotbarAutoFillConfig.java
+++ NeoForge/src/main/java/com/draconicvelum/hotbarautofill/HotbarAutoFillConfig.java
@@ -1,98 +1,39 @@
 package com.draconicvelum.hotbarautofill;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Properties;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
+import net.neoforged.neoforge.common.NeoForge;
 
-import net.minecraft.client.Minecraft;
+@Mod(value = HotbarAutoFillMod.MOD_ID, dist = Dist.CLIENT)
+public final class HotbarAutoFillMod {
+	static final String MOD_ID = "hotbarautofill";
 
-final class HotbarAutoFillConfig {
-	private static final String FILE_NAME = "hotbarautofill.properties";
-	private static final String REFILL_FROM_OTHER_HOTBAR_SLOTS = "refillFromOtherHotbarSlots";
-	private static final String PREVENT_TOOL_BREAKING = "preventToolBreaking";
-	private static final String SHOW_HELD_ITEM_TOTAL_COUNTER = "showHeldItemTotalCounter";
-	private static final boolean DEFAULT_REFILL_FROM_OTHER_HOTBAR_SLOTS = true;
-	private static final boolean DEFAULT_PREVENT_TOOL_BREAKING = false;
-	private static final boolean DEFAULT_SHOW_HELD_ITEM_TOTAL_COUNTER = true;
-
-	private final boolean refillFromOtherHotbarSlots;
-	private final boolean preventToolBreaking;
-	private final boolean showHeldItemTotalCounter;
-
-	private HotbarAutoFillConfig(
-			boolean refillFromOtherHotbarSlots,
-			boolean preventToolBreaking,
-			boolean showHeldItemTotalCounter
-	) {
-		this.refillFromOtherHotbarSlots = refillFromOtherHotbarSlots;
-		this.preventToolBreaking = preventToolBreaking;
-		this.showHeldItemTotalCounter = showHeldItemTotalCounter;
+	public HotbarAutoFillMod(ModContainer container) {
+		container.registerConfig(ModConfig.Type.CLIENT, HotbarAutoFillConfig.CLIENT_SPEC);
+		container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+		NeoForge.EVENT_BUS.addListener(this::onClientTick);
+		NeoForge.EVENT_BUS.addListener(this::onRenderGui);
 	}
 
-	static HotbarAutoFillConfig load() {
-		Path configPath = Minecraft.getInstance().gameDirectory.toPath().resolve("config").resolve(FILE_NAME);
-		Properties properties = new Properties();
-
-		if (Files.exists(configPath)) {
-			try (InputStream input = Files.newInputStream(configPath)) {
-				properties.load(input);
-			} catch (IOException ignored) {
-				return new HotbarAutoFillConfig(
-						DEFAULT_REFILL_FROM_OTHER_HOTBAR_SLOTS,
-						DEFAULT_PREVENT_TOOL_BREAKING,
-						DEFAULT_SHOW_HELD_ITEM_TOTAL_COUNTER
-				);
-			}
-		}
-
-		boolean refillFromOtherHotbarSlots = Boolean.parseBoolean(
-				properties.getProperty(
-						REFILL_FROM_OTHER_HOTBAR_SLOTS,
-						Boolean.toString(DEFAULT_REFILL_FROM_OTHER_HOTBAR_SLOTS)
-				)
-		);
-		boolean preventToolBreaking = Boolean.parseBoolean(
-				properties.getProperty(
-						PREVENT_TOOL_BREAKING,
-						Boolean.toString(DEFAULT_PREVENT_TOOL_BREAKING)
-				)
-		);
-		boolean showHeldItemTotalCounter = Boolean.parseBoolean(
-				properties.getProperty(
-						SHOW_HELD_ITEM_TOTAL_COUNTER,
-						Boolean.toString(DEFAULT_SHOW_HELD_ITEM_TOTAL_COUNTER)
-				)
-		);
-
-		properties.setProperty(REFILL_FROM_OTHER_HOTBAR_SLOTS, Boolean.toString(refillFromOtherHotbarSlots));
-		properties.setProperty(PREVENT_TOOL_BREAKING, Boolean.toString(preventToolBreaking));
-		properties.setProperty(SHOW_HELD_ITEM_TOTAL_COUNTER, Boolean.toString(showHeldItemTotalCounter));
-		saveDefaults(configPath, properties);
-		return new HotbarAutoFillConfig(refillFromOtherHotbarSlots, preventToolBreaking, showHeldItemTotalCounter);
+	private void onClientTick(ClientTickEvent.Pre event) {
+		configure();
+		HotbarRefill.tick(net.minecraft.client.Minecraft.getInstance());
 	}
 
-	boolean refillFromOtherHotbarSlots() {
-		return refillFromOtherHotbarSlots;
+	private void onRenderGui(RenderGuiEvent.Post event) {
+		configure();
+		HotbarItemCounter.render(event.getGuiGraphics());
 	}
 
-	boolean preventToolBreaking() {
-		return preventToolBreaking;
-	}
-
-	boolean showHeldItemTotalCounter() {
-		return showHeldItemTotalCounter;
-	}
-
-	private static void saveDefaults(Path configPath, Properties properties) {
-		try {
-			Files.createDirectories(configPath.getParent());
-			try (OutputStream output = Files.newOutputStream(configPath)) {
-				properties.store(output, "Hotbar Auto Fill client config");
-			}
-		} catch (IOException ignored) {
-		}
+	private static void configure() {
+		HotbarAutoFillConfig config = HotbarAutoFillConfig.load();
+		HotbarRefill.configure(config);
+		HotbarItemCounter.configure(config);
 	}
 }
